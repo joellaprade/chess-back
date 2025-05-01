e6a46fd3e47cd7974427afa80a3a7931750a53b8
Joe <joellaprade1@gmail.com>
2025-05-01 10:15:59 -0600

    before friends imp

--- src/config/app.ts
+++ src/config/app.ts
@@ -8,7 +8,7 @@ import routes from "../routes/index"
 
 const app = express()
 
-const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(',') || ""
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS
 
 const corsOptions = {
   origin: ALLOWED_ORIGINS,
@@ -19,18 +19,8 @@ const corsOptions = {
 app.use(helmet())
 app.use(cookieParser())
 app.use(cors(corsOptions))
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin) return callback(null, true); 
-//     if (ALLOWED_ORIGINS.includes(origin)) {
-//       return callback(null, true);
-//     } else {
-//       return callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// }));
+
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
--- src/controllers/fileManager.controller.ts
+++ src/controllers/fileManager.controller.ts
@@ -6,20 +6,17 @@ import { Session } from "../models/Session"
 
 export const updateUserProfilePicture = async (req: Request, res: Response) => {
   const filePath = req.file?.path
-  let imageUrl = "https://cdn.esawebb.org/archives/images/screen/carinanebula3.jpg";
-  // let imageUrl = "https://res.cloudinary.com/dd86ogsbh/image/upload/v1745984447/bdbzs0xmpshrddhjxbpg.jpg";
-  console.log(req.cookies)
+  let imageUrl = "";
   const userId: string = req.cookies.userId
   const user: User | null = await User.findById(userId)
   const session: Session | null = await Session.findOne({userId})
 
   if(filePath && user && session) {
-    // imageUrl = await handleCloudlyUpload(filePath)
+    imageUrl = await handleCloudlyUpload(filePath)
     user.image = imageUrl
     session.user.image = imageUrl
     await user.save();
     await session.save();
-    console.log(user)
   }
 
   res.send({message: "success"})
