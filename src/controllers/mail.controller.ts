5fd0c7907c86c451e7d39e9e4a6b9a62db8b1c69
Joe <joellaprade1@gmail.com>
2025-05-07 20:59:42 -0600

    working verification

--- src/controllers/mail.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express"
-import { sendMail } from "../config/mail"
-
-export const sendVerificationMail = (req: Request, res: Response) => {
-  const {email} = req.body;
-  const randomNum = Math.random() * 1000
-  sendMail(email, "Verifica Tu Correo", `https://10.45.1.112/${randomNum}`)
-}
--- src/routes/mail.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sendVerificationMail } from "../controllers/mail.controller";
-
-const router = Router()
-
-router.post('/verification', sendVerificationMail)
-
-export default router;
