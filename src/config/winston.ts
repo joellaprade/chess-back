6b489af28aa5046a467362f769d076ff771f517e
Joe <joellaprade1@gmail.com>
2025-06-11 21:46:11 -0600

    a

--- src/config/winston.ts
+++ src/config/winston.ts
@@ -10,7 +10,7 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: '/var/log/chess-backend-logs' })
+    new winston.transports.File({ filename: '/var/log/chess-backend-logs/app.log' })
   ]
 });
 
